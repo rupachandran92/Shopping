41b36e196862a0f955eb2940b803bfa322adbc62
Rupa Poosapati Chandran <rupa@Rupas-MacBook-Pro.local>
2020-04-18 20:33:58 -0700

    image details

---
+++ data.js
@@ -0,0 +1,207 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Eliana Pleated Dress Yellow",
+    img: [
+      {
+        id: 1,
+        image: "img/img11.jpg" 
+      },
+      {
+        id: 2,
+        image: "img/img12.jpg" 
+      },
+      {
+        id: 3,
+        image: "img/img13.jpg" 
+      }
+    ],
+    price: 10,
+    company: "Berrybenka",
+    info: "SPECIALLY MANUFACTURED BY TCL Made of smooth polyester blend, fully lined. Features accordion pleated details all round, comes in complimenting hues of marigold and white. Complete with an elasticised inset at the back for easy-wearing. Worn slightly loose, in an asymmetrical fit. Non sheer.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Farin Plaid Dress Multi Color",
+    img: [
+      {
+        id: 1,
+        image: "img/img21.jpg" 
+      },
+      {
+        id: 2,
+        image: "img/img22.jpg" 
+      },
+      {
+        id: 3,
+        image: "img/img23.jpg" 
+      }
+    ],
+    price: 16,
+    company: "Zalora",
+    info: "SPECIALLY MANUFACTURED BY TCL Made of smooth polyester blend, fully lined. Features accordion pleated details all round, comes in complimenting hues of marigold and white. Complete with an elasticised inset at the back for easy-wearing. Worn slightly loose, in an asymmetrical fit. Non sheer.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Eura Plaid Asymmetric Dress Green",
+    img: [
+      {
+        id: 1,
+        image: "img/img31.jpg" 
+      },
+      {
+        id: 2,
+        image: "img/img32.jpg" 
+      },
+      {
+        id: 3,
+        image: "img/img33.jpg" 
+      }
+    ],
+    price: 8,
+    company: "Uniqlo",
+    info: "SPECIALLY MANUFACTURED BY TCL Made of smooth polyester blend, fully lined. Features accordion pleated details all round, comes in complimenting hues of marigold and white. Complete with an elasticised inset at the back for easy-wearing. Worn slightly loose, in an asymmetrical fit. Non sheer.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Gloria Relax Dress Blue",
+    img: [
+      {
+        id: 1,
+        image: "img/img41.jpg" 
+      },
+      {
+        id: 2,
+        image: "img/img42.jpg" 
+      },
+      {
+        id: 3,
+        image: "img/img43.jpg" 
+      }
+    ],
+    price: 18,
+    company: "H&M",
+    info: "SPECIALLY MANUFACTURED BY TCL Made of smooth polyester blend, fully lined. Features accordion pleated details all round, comes in complimenting hues of marigold and white. Complete with an elasticised inset at the back for easy-wearing. Worn slightly loose, in an asymmetrical fit. Non sheer.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Elsie Tie V-Neck Dress Multi Color",
+    img: [
+      {
+        id: 1,
+        image: "img/img51.jpg" 
+      },
+      {
+        id: 2,
+        image: "img/img52.jpg" 
+      },
+      {
+        id: 3,
+        image: "img/img53.jpg" 
+      }
+    ],
+    price: 24,
+    company: "MANGO",
+    info: "SPECIALLY MANUFACTURED BY TCL Made of smooth polyester blend, fully lined. Features accordion pleated details all round, comes in complimenting hues of marigold and white. Complete with an elasticised inset at the back for easy-wearing. Worn slightly loose, in an asymmetrical fit. Non sheer.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Danika Plaid Dress Pink",
+    img: [
+      {
+        id: 1,
+        image: "img/img61.jpg" 
+      },
+      {
+        id: 2,
+        image: "img/img62.jpg" 
+      },
+      {
+        id: 3,
+        image: "img/img63.jpg" 
+      }
+    ],
+    price: 17,
+    company: "ZALORA",
+    info: "SPECIALLY MANUFACTURED BY TCL Made of smooth polyester blend, fully lined. Features accordion pleated details all round, comes in complimenting hues of marigold and white. Complete with an elasticised inset at the back for easy-wearing. Worn slightly loose, in an asymmetrical fit. Non sheer.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Frances Stripes Loose Tie Dress Yellow",
+    img: [
+      {
+        id: 1,
+        image: "img/img71.jpg" 
+      },
+      {
+        id: 2,
+        image: "img/img72.jpg" 
+      },
+      {
+        id: 3,
+        image: "img/img73.jpg" 
+      }
+    ],
+    price: 30,
+    company: "apple",
+    info: "SPECIALLY MANUFACTURED BY TCL Made of smooth polyester blend, fully lined. Features accordion pleated details all round, comes in complimenting hues of marigold and white. Complete with an elasticised inset at the back for easy-wearing. Worn slightly loose, in an asymmetrical fit. Non sheer.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Greta Loose Dress Black",
+    img: [
+      {
+        id: 1,
+        image: "img/img81.jpg" 
+      },
+      {
+        id: 2,
+        image: "img/img82.jpg" 
+      },
+      {
+        id: 3,
+        image: "img/img83.jpg" 
+      }
+    ],
+    price: 2,
+    company: "apple",
+    info: "SPECIALLY MANUFACTURED BY TCL Made of smooth polyester blend, fully lined. Features accordion pleated details all round, comes in complimenting hues of marigold and white. Complete with an elasticised inset at the back for easy-wearing. Worn slightly loose, in an asymmetrical fit. Non sheer.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info: "SPECIALLY MANUFACTURED BY TCL Made of smooth polyester blend, fully lined. Features accordion pleated details all round, comes in complimenting hues of marigold and white. Complete with an elasticised inset at the back for easy-wearing. Worn slightly loose, in an asymmetrical fit. Non sheer.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
